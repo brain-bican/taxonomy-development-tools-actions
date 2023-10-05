828f1c64fa7f2766af65852fa13b73f53e979421
hkir-dev <huseyin_kir@yahoo.com>
2023-10-05 11:50:19 +0100

    cli interface updated

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="tdta",
-    version="0.0.9.dev1",
+    version="0.0.10.dev1",
     description="The aim of this project is to provide taxonomy development tools custom actions.",
     long_description=README,
     long_description_content_type="text/markdown",
