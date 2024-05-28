21d22e8d393a4486d228dd28c7b65edb36289e4a
Huseyin Kir <73784267+hkir-dev@users.noreply.github.com>
2024-05-28 22:45:28 +0100

    ignore history on author annotations

--- src/tdta/tdt_export.py
+++ src/tdta/tdt_export.py
@@ -102,7 +102,7 @@ def parse_annotation_data(cta, sqlite_db, table_name):
                     author_annotation_fields = dict()
                     obj_fields = vars(annotation)
                     for column in columns:
-                        if column not in obj_fields and column not in ["row_number", "message"]:
+                        if column not in obj_fields and column not in ["row_number", "message", "history"]:
                             author_annotation_fields[column] = str(row[columns.index(column)])
                     annotation.author_annotation_fields = author_annotation_fields
 
